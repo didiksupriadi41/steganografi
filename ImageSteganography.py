c4d76efa2c85c83e051b7bbabf6dff152b5f094b
Didik Supriadi <didiksupriadi41@gmail.com>
2020-09-30 17:44:09 +0700

    Fix and update image steganography

--- ImageSteganography.py
+++ ImageSteganography.py
@@ -6,60 +6,70 @@ from PIL import Image
 
 
 def messageToBinary(message):
-    if type(message) == str:
-        return "".join([format(ord(i), "08b") for i in message])
-    elif type(message) == bytes or type(message) == np.ndarray:
-        return [format(i, "08b") for i in message]
-    elif type(message) == int or type(message) == np.uint8:
-        return format(message, "08b")
+    if type(message) == bytes or type(message) == np.ndarray:
+        return ''.join([format(i, "08b") for i in message])
+    elif type(message) == str:
+        return ''.join([format(ord(i), "08b") for i in message])
     else:
         raise TypeError("Input type not supported")
 
 
 class ImageSteganography:
     @staticmethod
-    def bit_depth(image_type):
-        if image_type == 'L' or image_type == 'P':
-            print("L")
-            bit_pixel = 8
-        elif image_type == 'RGB':
-            print("RGB")
-            bit_pixel = 24
-        elif image_type == 'RGBA':
-            print("RGBA")
-            bit_pixel = 32
-        else:
-            raise TypeError("Input type not supported")
-        return bit_pixel
-
-    @staticmethod
-    def hide_message(input_image, input_message, input_key=''):
+    def hide_message(input_choice, input_image, input_message, input_key=''):
         if input_image is None:
             raise ValueError("requires an input image")
         if input_message is None:
             raise ValueError("requires an input message")
 
+        with open(input_message, "rb") as f:
+            data = f.read()
         image = Image.open(input_image)
-        bit_pixel = ImageSteganography.bit_depth(image.mode)
-        data = open(input_message, "rb")
-        message = data.read()
+
+        bin_choice = messageToBinary(input_choice)
+        bin_data = messageToBinary(data)
+        message = bin_choice + bin_data
         print(message)
-        print(messageToBinary(message))
+
         ImageSteganography.lsb_method(image, message, input_key)
+        print(image.format)
         image.save("generated.png", "PNG")
+        image.close()
 
     @staticmethod
-    def lsb_method(image, message, key=''):
+    def bit_depth(image_type):
+        if image_type == 'L' or image_type == 'P':
+            return 1
+        elif image_type == 'RGB':
+            return 3
+        elif image_type == 'RGBA':
+            return 4
+        else:
+            raise TypeError("Input type not supported")
+
+    @staticmethod
+    def payload(input_message, input_bit, input_width, input_height):
+        if len(input_message) * 8 > input_bit * input_width * input_height:
+            return False
+        return True
+
+    @staticmethod
+    def lsb_method(input_image, input_message, input_key):
+        width, height = input_image.size
+        bit_pixel = ImageSteganography.bit_depth(input_image.mode)
+
+        if ImageSteganography.payload(input_message, bit_pixel, width, height):
+            raise TypeError('too many message')
+
         i = 0
-        width, height = image.size
         for x in range(0, width):
             for y in range(0, height):
-                pixel = list(image.getpixel((x, y)))
-                for n in range(0, 3):
-                    if i < len(message):
-                        pixel[n] = pixel[n] & ~1 | int(message[i])
+                pixel = list(input_image.getpixel((x, y)))
+                for n in range(0, bit_pixel):
+                    if i < len(input_message):
+                        pixel[n] = pixel[n] & ~1 | int(input_message[i])
                         i += 1
-                image.putpixel((x, y), tuple(pixel))
+                input_image.putpixel((x, y), tuple(pixel))
 
     @staticmethod
     def bpcs_method(image, message, key=''):
@@ -67,5 +77,25 @@ class ImageSteganography:
 
 
 if __name__ == "__main__":
-    ImageSteganography.hide_message("a.png", "a.txt", "didik")
+
+    print("Penyisipan pesan")
+    print("  tanpa enkripsi")
+    # print("  dengan enkripsi")
+    # enc = input()
+
+    print("  metode lsb")
+    print("    pixel-pixel sekuensial")
+    # print("    pixel-pixel acak")
+    # print("  metode bpcs")
+    # print("    blok-blok sekuensial")
+    # print("    blok-blok acak")
+
+    # print("ekstraksi pesan")
+    # print("  metode lsb")
+    # print("  metode bpcs")
+
+    input_choice = input()
+    if input_choice[0] == '1':
+        if input_choice[1] == '1':
+            ImageSteganography.hide_message(input_choice, "save.png", "a.txt", "didik")
     pass
